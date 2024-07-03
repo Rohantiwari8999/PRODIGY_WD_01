efc5c0ff744be74a79ccdceac1f5733f38a8a631
Rohantiwari8999 <151922640+Rohantiwari8999@users.noreply.github.com>
2024-07-03 23:13:12 +0530

    Prodigy Infotech task- 1

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+      var header = document.querySelector('header');
+      header.classList.toggle('scrolled', window.scrollY > 0);
+    });
+  });
+  
